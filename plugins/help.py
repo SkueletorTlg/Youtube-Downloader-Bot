43d32b32660eee4457a3eb5bae5faba138625101
SkueletorTlg <73276191+SkueletorTlg@users.noreply.github.com>
2020-12-09 23:09:49 -0400

    Update help.py

--- plugins/help.py
+++ plugins/help.py
@@ -3,5 +3,5 @@ from pyrogram import Client, Filters
 
 @Client.on_message(Filters.command(["help"]))
 async def start(client, message):
-    helptxt = f"Currently Only supports Youtube Single  (No playlist) Just Send Youtube Url"
+    helptxt = f"Actualmente el bot sólo es compatible con YouTube  (No lista de reproducción) Simplemente envía el link de YouTube ☺️"
     await message.reply_text(helptxt)
